66b72436ff2269f5db2776cc9d3eea92c2086764
donutkrll <kirill.babich2004@gmail.com>
2024-06-02 15:34:39 +0300

    program

---
+++ Lab1ControlLibrary/MyCustomControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1ControlLibrary
+{
+    [ToolboxBitmap(@"D:\KHAI\apz\lab1\Lab1Solution\Lab1ControlLibrary\Icon1.ico")]
+    public partial class MyCustomControl : TextBox
+    {
+        public MyCustomControl()
+        {
+            InitializeComponent();
+            this.TextChanged += MyCustomControl_TextChanged;
+            this.ForeColor = InvalidValueColor;
+        }
+
+        public float FloatValue
+        {
+            get
+            {
+                float value;
+                if (float.TryParse(this.Text, out value))
+                {
+                    return value;
+                }
+                return 0;
+            }
+        }
+
+        public Color InvalidValueColor { get; set; } = Color.Red;
+
+        public event EventHandler CorrectValueEntered;
+
+        public event EventHandler IncorrectValueEntered;
+
+        private void MyCustomControl_TextChanged(object sender, EventArgs e)
+        {
+            if (!this.Text.Contains(","))
+            {
+                this.ForeColor = InvalidValueColor;
+                IncorrectValueEntered?.Invoke(this, EventArgs.Empty);
+                return;
+            }
+
+            float value;
+            if (!float.TryParse(this.Text, out value))
+            {
+                this.ForeColor = InvalidValueColor;
+                IncorrectValueEntered?.Invoke(this, EventArgs.Empty);
+                return;
+            }
+
+
+            this.ForeColor = Color.Black;
+            CorrectValueEntered?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Lab1WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Lab1WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            myCustomControl1 = new Lab1ControlLibrary.MyCustomControl();
+            SuspendLayout();
+            // 
+            // myCustomControl1
+            // 
+            myCustomControl1.Font = new Font("Segoe UI", 16F);
+            myCustomControl1.ForeColor = Color.Red;
+            myCustomControl1.InvalidValueColor = Color.Red;
+            myCustomControl1.Location = new Point(81, 121);
+            myCustomControl1.Name = "myCustomControl1";
+            myCustomControl1.Size = new Size(234, 36);
+            myCustomControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(403, 380);
+            Controls.Add(myCustomControl1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Lab1ControlLibrary.MyCustomControl myCustomControl1;
+    }
+}
